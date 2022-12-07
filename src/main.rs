087b76a3a9d70571700a6bdc40673e23579eae01
João Gabriel L. Romão <gabriel.lacerda@engenharia.ufjf.br>
2022-12-06 21:56:39 -0300

    test new filters

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,55 @@
+mod dft;
+mod irr_filter;
 mod moving_average;
 mod savitzky_golay;
 
-use std::env::current_dir;
-// use std::{env, io};
-use std::io::prelude::*;
-use std::{fs::File, io::BufReader};
+use std::f64::consts::PI;
 
 use moving_average::MovingAverage;
 use savitzky_golay::SavitzkyGolay;
+
+use crate::dft::DFT;
+use crate::irr_filter::IIRFilter;
 
 fn main() {
     // env::set_var("RUST_BACKTRACE", "full");
 
     let mut sg: SavitzkyGolay<17> = SavitzkyGolay::new(960);
-    println!("{:?}", sg);
+    // println!("{:?}", sg);
 
-    // let mut mov_avg: MovingAverage<16> = MovingAverage::new();
+    let iir_num = [
+        0.000024419565298361,
+        0.000097678261193446,
+        0.000146517391790169,
+        0.000097678261193446,
+        0.000024419565298361,
+    ];
+    let iir_den = [
+        1.000000000000000,
+        -3.615352702016196,
+        4.918315719139102,
+        -2.982871345869219,
+        0.680299041791087,
+    ];
+    let mut iir: IIRFilter<5, 5> = IIRFilter::new(&iir_num, &iir_den);
 
-    let samples: [f64; 20] = [10.0; 20];
+    let mut dft1: DFT<16, { (16.0 * 10.25) as usize }> = DFT::new(960);
+    let mut dft2: DFT<16, { (16.0 * 10.25) as usize }> = DFT::new(960);
 
-    for sample in samples {
+    let mut mov_avg: MovingAverage<16> = MovingAverage::new();
+
+    // let samples: [f64; 200] = [10.0; 200];
+
+    println!("{:#?}", dft1);
+    println!("{:#?}", dft2);
+    for sample in 0..200 {
         // println!("{}, {}", sample, mov_avg.filter(sample));
-        println!("{:?}", sg.filter(sample));
+        // println!("{:?}", iir.filter(sample));
+        dft1.calculate(10000.0 * (2.0 * PI * 60.0 * sample as f64 / (960.0)).cos());
+        print!("mag: {} | ", (dft1.im * dft1.im + dft1.re * dft1.re).sqrt());
+        // println!("============================================ normal");
+        dft2.calculate_symmetric(10000.0 * (2.0 * PI * 60.0 * sample as f64 / (960.0)).cos());
+        println!("mag: {}", (dft2.im * dft2.im + dft2.re * dft2.re).sqrt());
+        // println!("============================================ simetrico");
     }
 }
